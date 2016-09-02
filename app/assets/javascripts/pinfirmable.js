2795c2714954472730a0b3e3562f4bf44c081ecd
Greg Woodcock <greg@yoomee.com>
2016-09-02 15:04:17 +0100

    Improve javascript for autotabbing

--- app/assets/javascripts/pinfirmable.js
+++ app/assets/javascripts/pinfirmable.js
@@ -1,24 +1,85 @@
 var pinfirmable = {
-  autoTab: function(event) {
-    if(!isNaN(event.key - parseFloat(event.key))) {
-      var elem = event.srcElement;
-      var i;
-      var elements = elem.form.elements;
-        for (i=0, numElements=elements.length; i<numElements; i++) {
-          if (elements[i]==elem) {
-            break;
-        }
-      }
-      var nextElem = elements[i+1];
-      if(nextElem.type == "text" || nextElem.type == "number") {
-        elements[i+1].focus();
-      }
-      else {
-        elem.form.submit();
-      }
-      return true;
-    } else {
-      return false;
+  init: function() {
+    var elem = document.getElementById('pinfirmable-noscript');
+    if(elem) {
+      elem.style.display = 'none';
     }
+  },
+
+  autoTab: function(event) {
+    // event.preventDefault();
+    switch (this.detectKeyPress(event)) {
+    case "digit":
+      this.handleDigitPress(event);
+      event.preventDefault();
+      break;
+    case "backspace":
+      this.handleBackspacePress(event);
+      break;
+    case "character":
+      return false;
+    default:
+      return true;
+    }
+  },
+
+  handleDigitPress: function(event) {
+    var elem = event.srcElement;
+    if (elem.value.length === 1) {
+      elem.value = event.key;
+    } else {
+      elem.value = event.key;
+    }
+
+    var nextElem = this.nextElement(elem);
+    if(nextElem.type == "text" || nextElem.type == "number") {
+      nextElem.focus();
+    }
+    else {
+      elem.form.submit();
+    }
+    return true;
+  },
+
+  handleBackspacePress: function(event) {
+    var elem = event.srcElement;
+    if(elem.value.length > 0) {
+      elem.value = "";
+    } else {
+      var previousElement = this.previousElement(elem);
+      previousElement.value = "";
+      previousElement.focus();
+    }
+  },
+
+  indexOfElement: function(elem) {
+    var i;
+    var elements = elem.form.elements;
+      for (i=0, numElements=elements.length; i<numElements; i++) {
+        if (elements[i]==elem) {
+          break;
+      }
+    }
+    return i;
+  },
+
+  nextElement: function(elem) {
+    var elements = elem.form.elements;
+    var i = this.indexOfElement(elem);
+    return elements[i + 1];
+  },
+
+  previousElement: function(elem) {
+    var elements = elem.form.elements;
+    var i = this.indexOfElement(elem);
+    return elements[i - 1];
+  },
+
+  detectKeyPress: function(event) {
+    if(event.keyCode === 8) { return "backspace"; }
+    if(!isNaN(event.key - parseFloat(event.key))) { return "digit"; }
+    if(/\S/.test(String.fromCharCode(event.keyCode))) { return "character"; }
   }
 };
+
+pinfirmable.init();
