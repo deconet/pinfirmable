9a29af667b9bf5d418ab42cd4f02fda9719d3eb6
Greg Woodcock <greg@yoomee.com>
2016-09-02 15:28:10 +0100

    Add email subject to translations

--- app/mailers/pinfirmable_mailer.rb
+++ app/mailers/pinfirmable_mailer.rb
@@ -1,5 +1,11 @@
 class PinfirmableMailer < ApplicationMailer
   def pin_email(user)
-    mail(to: user.email, subject: "Confirmation code: #{user.pinfirmable_pin[0..2]}-#{user.pinfirmable_pin[3..6]}")
+    mail(
+      to: user.email,
+      subject: default_i18n_subject(
+        code_part1: user.pinfirmable_pin[0..2],
+        code_part2: user.pinfirmable_pin[3..6]
+      )
+    )
   end
 end
