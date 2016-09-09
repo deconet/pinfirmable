5e3f02a6f3a51f6f1360486c0cd685b571fc8c80
Ant Barnes <ant@yoomee.com>
2016-09-09 16:25:38 +0100

    Fix hound violations

--- spec/models/pinfirmable_spec.rb
+++ spec/models/pinfirmable_spec.rb
@@ -1,14 +1,15 @@
+# frozend_string_literal: true
 require 'spec_helper'
 
 describe Pinfirmable do
+  it 'calls emailer after create' do
+    expect(PinfirmableMailer)
+    .to receive(:pin_email).with(an_instance_of(User))
+    .and_return( double('PinfirmableMailer', deliver: true))
 
-  it "calls emailer after create" do
-    expect(PinfirmableMailer).
-      to receive(:pin_email).with(an_instance_of(User)).
-        and_return( double('PinfirmableMailer', deliver: true))
     u = User.new(
-              email: "test@example.com",
-              password: "password"
+              email: 'test@example.com',
+              password: 'password'
     )
     u.save
   end
@@ -17,8 +18,8 @@ describe Pinfirmable do
     expect(PinfirmableMailer).
       not_to receive(:pin_email).with(an_instance_of(User))
     u = User.new(
-      email: "test@example.com",
-      password: "password"
+      email: 'test@example.com',
+      password: 'password'
     )
     u.skip_pinfirmation!
     u.save
