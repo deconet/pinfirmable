af4cbe7a7b315aa408aace88f875e65066f597d5
Ant Barnes <ant@yoomee.com>
2016-09-12 10:58:47 +0100

    Fix style violations

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -31,5 +31,4 @@ ActiveRecord::Schema.define(version: 20160901131628) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
