b6763092ab924680a8a22a2bdc2f5679ae8dc164
Greg Woodcock <greg@yoomee.com>
2016-11-23 09:36:40 +0000

    Fix cross browser autotabbing when entering pin

--- app/assets/javascripts/pinfirmable.js
+++ app/assets/javascripts/pinfirmable.js
@@ -6,28 +6,39 @@ var pinfirmable = {
     }
   },
 
+  pastePin: function(event) {
+    event.preventDefault();
+    var clipboardData = event.clipboardData || window.clipboardData;
+    var pastedData = clipboardData.getData('Text');
+    var elemIndex = this.indexOfElement(event.currentTarget);
+    var elements = event.currentTarget.form.getElementsByClassName("pin-input");
+    var pasteCounter = 0;
+    for(var i = elemIndex; i < pastedData.length; i++) {
+      elements[i].value = pastedData[pasteCounter];
+      elements[i].focus();
+      pasteCounter++;
+    }
+  },
+
   autoTab: function(event) {
+    event.preventDefault();
     switch (this.detectKeyPress(event)) {
-    case "digit":
-      this.handleDigitPress(event);
-      event.preventDefault();
-      break;
-    case "backspace":
-      this.handleBackspacePress(event);
-      break;
-    case "character":
-      return false;
-    default:
-      return true;
+      case "digit":
+        this.handleDigitPress(event);
+        break;
+      case "backspace":
+        this.handleBackspacePress(event);
+        break;
     }
   },
 
   handleDigitPress: function(event) {
     var elem = event.currentTarget;
-    elem.value = event.key;
+    var key = this.getKeyCharacter(event);
+    elem.value = key;
 
     var nextElem = this.nextElement(elem);
-    if(nextElem.type == "text" || nextElem.type == "number") {
+    if(nextElem && (nextElem.type == "text" || nextElem.type == "number" || nextElem.type == "tel")) {
       nextElem.focus();
     }
     else {
@@ -37,6 +48,8 @@ var pinfirmable = {
   },
 
   handleBackspacePress: function(event) {
+    var key = this.getKeyCharacter(event);
+    if(key !== "Backspace") { return; } else {
     var elem = event.currentTarget;
     if(elem.value.length > 0) {
       elem.value = "";
@@ -44,12 +57,12 @@ var pinfirmable = {
       var previousElement = this.previousElement(elem);
       previousElement.value = "";
       previousElement.focus();
-    }
+    }}
   },
 
   indexOfElement: function(elem) {
     var i;
-    var elements = elem.form.elements;
+    var elements = elem.form.getElementsByClassName("pin-input");
       for (i=0, numElements=elements.length; i<numElements; i++) {
         if (elements[i]==elem) {
           break;
@@ -59,21 +72,37 @@ var pinfirmable = {
   },
 
   nextElement: function(elem) {
-    var elements = elem.form.elements;
+    var elements = elem.form.getElementsByClassName("pin-input");
     var i = this.indexOfElement(elem);
     return elements[i + 1];
   },
 
   previousElement: function(elem) {
-    var elements = elem.form.elements;
+    var elements = elem.form.getElementsByClassName("pin-input");
     var i = this.indexOfElement(elem);
     return elements[i - 1];
   },
 
   detectKeyPress: function(event) {
-    if(event.keyCode === 8) { return "backspace"; }
-    if(!isNaN(event.key - parseFloat(event.key))) { return "digit"; }
-    if(/\S/.test(String.fromCharCode(event.keyCode))) { return "character"; }
+    var key = this.getKeyCharacter(event);
+    if(!isNaN(key - parseFloat(key))) { return "digit"; }
+    if(/\S/.test(key)) { return "character"; }
+  },
+
+  getKeyCharacter: function(event) {
+    if(event.metaKey || event.ctrlKey) {
+      return false;
+    }else {
+      if (event.key) {
+        return event.key;
+      }
+      else if(event.keyCode === 8) {
+        return "Backspace";
+      }
+      else {
+        return String.fromCharCode(event.keyCode);
+      }
+    }
   }
 };
 
