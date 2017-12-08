8d2f2c286c05376536a15b7445c6285d64432ecf
James Jefferies <james@shedcode.co.uk>
2017-12-08 17:52:43 +0000

    Tabs?! Argh

--- db/migrate/20160901131628_add_pinfirmable_columns_to_resource.rb
+++ db/migrate/20160901131628_add_pinfirmable_columns_to_resource.rb
@@ -2,9 +2,9 @@ class AddPinfirmableColumnsToResource < ActiveRecord::Migration[5.0]
   def change
     model = (ENV["MODEL"] || "user").downcase.pluralize.to_sym
     unless column_exists?(model, :pinfirmable_pin)
-    	add_column model, :pinfirmable_pin, :string
-    	add_column model, :pinfirmable_tries, :integer, default: 0
-    	add_column model, :pinfirmable_lockout, :datetime, default: nil
-		end
+      add_column model, :pinfirmable_pin, :string
+      add_column model, :pinfirmable_tries, :integer, default: 0
+      add_column model, :pinfirmable_lockout, :datetime, default: nil
+    end
   end
 end
